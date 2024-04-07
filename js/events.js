39a5b979ad2360b09566e4ebd8b6b7bc242d92d2
John Durtche <johndurtche@gmail.com>
2024-04-07 10:37:50 -0400

    New Slide-Container layout system added

--- js/events.js
+++ js/events.js
@@ -21,6 +21,36 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+// Smooth scroll and mouse-draggable media for slide-container
+const slider = document.querySelector('.slide-container');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', (e) => {
+  if(!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3; //scroll-fast
+  slider.scrollLeft = scrollLeft - walk;
+  console.log(walk);
+});
+
 // Get references to the modal and button elements
 const modal = document.getElementById('notificationModal');
 const showModalButton = document.getElementById('showNotification');
